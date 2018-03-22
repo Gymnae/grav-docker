ad4317d8617f71033a26e8850e9b14e5dd5921cd
Gymnae <dagorwaith@gmail.com>
2018-03-22 14:06:28 +0100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if ! [ -e index.php -a -e bin/grav ]; then
   echo "Required files not found in $PWD - Copying from ${SOURCE}..."
   cp -r "$SOURCE"/. $PWD
-  chown -R www-data:www-data $PWD
+  chown -R nginx:www-data $PWD
   echo "Done."
 fi
 
